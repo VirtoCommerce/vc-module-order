e396c5243715feaad3aad815e9532e54137567d3
Tatarincev <et@virtoway.com>
2016-06-29 16:40:56 +0200

    Move /api/paymentcallback method from Core module

---
+++ VirtoCommerce.OrderModule.Web/Model/PaymentCallbackParameters.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+
+namespace VirtoCommerce.OrderModule.Web.Model
+{
+    public class PaymentCallbackParameters
+    {
+        public KeyValuePair[] Parameters { get; set; }
+    }
+}
