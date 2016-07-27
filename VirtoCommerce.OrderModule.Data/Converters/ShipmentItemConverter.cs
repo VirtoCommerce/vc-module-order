29d7a5e0b8ebc6939efe5638b468b1a4457a92fe
eMazeika <em@virtoway.com>
2016-07-27 12:13:40 +0300

    shipmentItem converter fixed

--- VirtoCommerce.OrderModule.Data/Converters/ShipmentItemConverter.cs
+++ VirtoCommerce.OrderModule.Data/Converters/ShipmentItemConverter.cs
@@ -13,42 +13,42 @@ using VirtoCommerce.Platform.Data.Common.ConventionInjections;
 
 namespace VirtoCommerce.OrderModule.Data.Converters
 {
-	public static class ShipmentItemConverter
-	{
-		public static ShipmentItem ToCoreModel(this ShipmentItemEntity entity)
-		{
-			if (entity == null)
-				throw new ArgumentNullException("entity");
+    public static class ShipmentItemConverter
+    {
+        public static ShipmentItem ToCoreModel(this ShipmentItemEntity entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException("entity");
 
-			var retVal = new ShipmentItem();
-			retVal.InjectFrom(entity);
+            var retVal = new ShipmentItem();
+            retVal.InjectFrom(entity);
 
-			if(entity.LineItem != null)
-			{
-				retVal.LineItem = entity.LineItem.ToCoreModel();
-			}
-			return retVal;
-		}
+            if (entity.LineItem != null)
+            {
+                retVal.LineItem = entity.LineItem.ToCoreModel();
+            }
+            return retVal;
+        }
 
-		public static ShipmentItem ToOrderCoreModel(this cartCoreModel.ShipmentItem shipmentItem)
-		{
-			if (shipmentItem == null)
-				throw new ArgumentNullException("shipmentItem");
+        public static ShipmentItem ToOrderCoreModel(this cartCoreModel.ShipmentItem shipmentItem)
+        {
+            if (shipmentItem == null)
+                throw new ArgumentNullException("shipmentItem");
 
-			var retVal = new ShipmentItem();
-			retVal.InjectFrom(shipmentItem);
+            var retVal = new ShipmentItem();
+            retVal.InjectFrom(shipmentItem);
 
-			retVal.LineItem = shipmentItem.LineItem.ToOrderCoreModel();
-			return retVal;
-		}
+            retVal.LineItem = shipmentItem.LineItem.ToOrderCoreModel();
+            return retVal;
+        }
 
 
-		public static ShipmentItemEntity ToDataModel(this ShipmentItem shipmentItem, CustomerOrderEntity orderEntity, PrimaryKeyResolvingMap pkMap)
-		{
-			if (shipmentItem == null)
-				throw new ArgumentNullException("shipmentItem");
+        public static ShipmentItemEntity ToDataModel(this ShipmentItem shipmentItem, CustomerOrderEntity orderEntity, PrimaryKeyResolvingMap pkMap)
+        {
+            if (shipmentItem == null)
+                throw new ArgumentNullException("shipmentItem");
 
-			var retVal = new ShipmentItemEntity();
+            var retVal = new ShipmentItemEntity();
             pkMap.AddPair(shipmentItem, retVal);
             retVal.InjectFrom(shipmentItem);
 
@@ -57,36 +57,36 @@ namespace VirtoCommerce.OrderModule.Data.Converters
             {
                 retVal.LineItem = orderEntity.Items.FirstOrDefault(x => x.Id == shipmentItem.LineItemId);
             }
-            if (retVal.LineItem == null && shipmentItem.LineItem != null)
+            if (retVal.LineItem == null && shipmentItem.LineItem != null && !string.IsNullOrEmpty(shipmentItem.LineItem.Id))
             {
                 retVal.LineItem = orderEntity.Items.FirstOrDefault(x => x.Id == shipmentItem.LineItem.Id);
             }
-            if (retVal.LineItem == null && shipmentItem.LineItem != null)
+            if (retVal.LineItem == null && shipmentItem.LineItem != null && !string.IsNullOrEmpty(shipmentItem.LineItem.ProductId))
             {
                 retVal.LineItem = orderEntity.Items.FirstOrDefault(x => x.ProductId == shipmentItem.LineItem.ProductId);
             }
-            if (retVal.LineItem != null && !String.IsNullOrEmpty(retVal.LineItem.Id))
+            if (retVal.LineItem != null && !string.IsNullOrEmpty(retVal.LineItem.Id))
             {
                 retVal.LineItemId = retVal.LineItem.Id;
                 retVal.LineItem = null;
             }
             return retVal;
-		}
+        }
 
-		/// <summary>
-		/// Patch CatalogBase type
-		/// </summary>
-		/// <param name="source"></param>
-		/// <param name="target"></param>
-		public static void Patch(this ShipmentItemEntity source, ShipmentItemEntity target)
-		{
-			if (target == null)
-				throw new ArgumentNullException("target");
+        /// <summary>
+        /// Patch CatalogBase type
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="target"></param>
+        public static void Patch(this ShipmentItemEntity source, ShipmentItemEntity target)
+        {
+            if (target == null)
+                throw new ArgumentNullException("target");
 
 
-			var patchInjectionPolicy = new PatchInjection<ShipmentItemEntity>(x => x.BarCode, x => x.ShipmentId, x => x.ShipmentPackageId, x=>x.Quantity);
-			target.InjectFrom(patchInjectionPolicy, source);
-		}
+            var patchInjectionPolicy = new PatchInjection<ShipmentItemEntity>(x => x.BarCode, x => x.ShipmentId, x => x.ShipmentPackageId, x => x.Quantity);
+            target.InjectFrom(patchInjectionPolicy, source);
+        }
 
-	}
+    }
 }
