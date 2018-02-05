36623f731fbbf482d6b1b4e0f7df536c4f6e0be1
Yuliya Malik <yuliya.umbrellagroup@gmail.com>
2018-02-05 22:05:56 +0200

    getting module connection string changed

--- VirtoCommerce.OrderModule.Web/Module.cs
+++ VirtoCommerce.OrderModule.Web/Module.cs
@@ -33,7 +33,7 @@ namespace VirtoCommerce.OrderModule.Web
 {
     public class Module : ModuleBase, ISupportExportImportModule
     {
-        private readonly string _connectionStringName = ConfigurationHelper.GetConnectionStringValue("{{ModuleId}}") ?? ConfigurationHelper.GetConnectionStringValue("VirtoCommerce");
+        private readonly string _connectionStringName = ConfigurationHelper.GetConnectionStringValue("VirtoCommerce.Orders") ?? ConfigurationHelper.GetConnectionStringValue("VirtoCommerce");
         private readonly IUnityContainer _container; 
 
         public Module(IUnityContainer container)
