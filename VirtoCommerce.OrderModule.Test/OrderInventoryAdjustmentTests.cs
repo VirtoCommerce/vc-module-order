9c84e83a8c488ddd8033b8698d8162f6cac756f1
Andrey A. Vasenev <avas.the.wise@gmail.com>
2018-11-09 17:32:49 +0700

    69: concurrency fix for inventory adjustment (#70)
    * Added Hangfire.Core package to the OrderModule.Data project.
    
    * AdjustInventoryOrderChangedEventHandler: added Hangfire job to do actual inventory adjustment, replaced GenericChangedEntry<CustomerOrder> with actual data needed for adjustment.
    
    * Removed the AdjustOrderInventoryJobArgs - it was used only to pass arguments to the Hangfire job, so it isn't really needed there.
    
    * AdjustInventoryOrderChangedEventHandler: reduced amount of data passed to the background job, simplified the code executing in it.
    
    * Added warning for setting that enables inventory adjustment, as it may cause unexpected in-stock quantity increase in some cases.
    
    * Fixed a typo in setting description.
    
    * Removed the entryState parameter from the TryAdjustOrderInventoryBackgroundJob, because it isn't used anymore.
    
    * Fix the two breaking changes bugs that causes to invalid logic of inventory adjustment
    Some code polishing and renaming
    
    * Added some unit tests for AdjustInventoryOrderChangedEventHandler.
    
    * Removed the IBackgroundJobClient from the constructor of AdjustInventoryOrderChangedEventHandler and added HandleImmediately method to still be able to unit-test order changed event handling.
    
    * AdjustInventoryOrderChangedEventHandler: removed HandleImmediately() method, replaced complex tests of complete event handling with data-driven tests of getting product inventory changes for order change.
    
    * Order inventory adjustment test: simplified comparer creation a bit.
    
    * Order inventory adjustment test: added a category definition for the test class.

---
+++ VirtoCommerce.OrderModule.Test/OrderInventoryAdjustmentTests.cs
@@ -0,0 +1,154 @@
+using System;
+using System.Collections.Generic;
+using Moq;
+using VirtoCommerce.Domain.Common.Events;
+using VirtoCommerce.Domain.Inventory.Services;
+using VirtoCommerce.Domain.Order.Model;
+using VirtoCommerce.Domain.Store.Model;
+using VirtoCommerce.Domain.Store.Services;
+using VirtoCommerce.OrderModule.Data.Handlers;
+using VirtoCommerce.Platform.Core.Common;
+using VirtoCommerce.Platform.Core.Settings;
+using Xunit;
+
+namespace VirtoCommerce.OrderModule.Test
+{
+    [CLSCompliant(false)]
+    [Trait("Category", "CI")]
+    public class OrderInventoryAdjustmentTests
+    {
+        private const string TestStoreId = "TestStore";
+        private const string TestFulfillmentCenterId = "TestFulfillmentCenter";
+
+        private static readonly CustomerOrder InitialOrder = new CustomerOrder
+        {
+            StoreId = TestStoreId,
+            Items = new List<LineItem>
+            {
+                new LineItem
+                {
+                    Id = "{01234567-89ab-cdef-0123-456789abcdef}",
+                    ProductId = "shoes",
+                    Quantity = 2
+                },
+                new LineItem
+                {
+                    Id = "{abcdef01-2345-6789-abcd-ef0123456789}",
+                    ProductId = "t-shirt",
+                    Quantity = 2
+                }
+            }
+        };
+
+        private static readonly CustomerOrder ChangedOrder = new CustomerOrder
+        {
+            StoreId = TestStoreId,
+            Items = new List<LineItem>
+            {
+                new LineItem
+                {
+                    Id = "{01234567-89ab-cdef-0123-456789abcdef}",
+                    ProductId = "shoes",
+                    Quantity = 5
+                },
+                new LineItem
+                {
+                    Id = "{fedcba98-7654-3210-0123-456789abcdef}",
+                    ProductId = "t-shirt-new",
+                    Quantity = 1
+                }
+            }
+        };
+
+        public static readonly IList<object[]> TestData = new List<object[]>
+        {
+            new object[]
+            {
+                new GenericChangedEntry<CustomerOrder>(ChangedOrder, InitialOrder, EntryState.Modified),
+                new[]
+                {
+                    new AdjustInventoryOrderChangedEventHandler.ProductInventoryChange
+                    {
+                        FulfillmentCenterId = TestFulfillmentCenterId,
+                        ProductId = "shoes",
+                        QuantityDelta = 3
+                    },
+                    new AdjustInventoryOrderChangedEventHandler.ProductInventoryChange
+                    {
+                        FulfillmentCenterId = TestFulfillmentCenterId,
+                        ProductId = "t-shirt-new",
+                        QuantityDelta = 1
+                    },
+                    new AdjustInventoryOrderChangedEventHandler.ProductInventoryChange
+                    {
+                        FulfillmentCenterId = TestFulfillmentCenterId,
+                        ProductId = "t-shirt",
+                        QuantityDelta = -2
+                    }
+                }
+            },
+            new object[]
+            {
+                new GenericChangedEntry<CustomerOrder>(InitialOrder, InitialOrder, EntryState.Added),
+                new[]
+                {
+                    new AdjustInventoryOrderChangedEventHandler.ProductInventoryChange
+                    {
+                        FulfillmentCenterId = TestFulfillmentCenterId,
+                        ProductId = "shoes",
+                        QuantityDelta = 2
+                    },
+                    new AdjustInventoryOrderChangedEventHandler.ProductInventoryChange
+                    {
+                        FulfillmentCenterId = TestFulfillmentCenterId,
+                        ProductId = "t-shirt",
+                        QuantityDelta = 2
+                    }
+                }
+            },
+            new object[]
+            {
+                new GenericChangedEntry<CustomerOrder>(InitialOrder, InitialOrder, EntryState.Deleted),
+                new[]
+                {
+                    new AdjustInventoryOrderChangedEventHandler.ProductInventoryChange
+                    {
+                        FulfillmentCenterId = TestFulfillmentCenterId,
+                        ProductId = "shoes",
+                        QuantityDelta = -2
+                    },
+                    new AdjustInventoryOrderChangedEventHandler.ProductInventoryChange
+                    {
+                        FulfillmentCenterId = TestFulfillmentCenterId,
+                        ProductId = "t-shirt",
+                        QuantityDelta = -2
+                    }
+                }
+            }
+        };
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void AdjustInventoryHandler_GetProductInventoryChanges_ForOrderChanges(GenericChangedEntry<CustomerOrder> orderChangedEntry,
+            IEnumerable<AdjustInventoryOrderChangedEventHandler.ProductInventoryChange> expectedChanges)
+        {
+            // Arrange
+            var inventoryServiceMock = new Mock<IInventoryService>();
+            var settingsManagerMock = new Mock<ISettingsManager>();
+
+            var storeServiceMock = new Mock<IStoreService>();
+            storeServiceMock.Setup(x => x.GetById(TestStoreId))
+                .Returns(new Store { MainFulfillmentCenterId = TestFulfillmentCenterId });
+
+            var targetHandler = new AdjustInventoryOrderChangedEventHandler(inventoryServiceMock.Object,
+                storeServiceMock.Object, settingsManagerMock.Object);
+
+            // Act
+            var actualChanges = targetHandler.GetProductInventoryChangesFor(orderChangedEntry);
+            
+            // Assert
+            var equalityComparer = AnonymousComparer.Create((AdjustInventoryOrderChangedEventHandler.ProductInventoryChange x) => $"{x.FulfillmentCenterId} {x.ProductId} {x.QuantityDelta}");
+            Assert.Equal(expectedChanges, actualChanges, equalityComparer);
+        }
+    }
+}
