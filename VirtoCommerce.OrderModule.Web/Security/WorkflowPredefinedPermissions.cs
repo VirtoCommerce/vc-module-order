f30a1daf68074b4b3ae51ee0b6f7ca7863e24f98
NITECO\vinh.vo <vinh.vo@niteco.se>
2019-03-21 14:35:55 +0700

    vt-20 #create api controller

---
+++ VirtoCommerce.OrderModule.Web/Security/WorkflowPredefinedPermissions.cs
@@ -0,0 +1,8 @@
+namespace VirtoCommerce.OrderModule.Web.Security
+{
+    public static class WorkflowPredefinedPermissions
+    {
+        public const string Read = "workflow:read",
+                            Upload = "workflow:upload";
+    }
+}
