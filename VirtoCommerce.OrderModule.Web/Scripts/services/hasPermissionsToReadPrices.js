f92e3126b284998ccefeb7d6e8bdf7c031c46476
Makarov Artem <halfelkan@mail.ru>
2019-03-25 09:11:10 +0300

    Changed the check to show prices

--- VirtoCommerce.OrderModule.Web/Scripts/services/hasPermissionsToReadPrices.js
+++ VirtoCommerce.OrderModule.Web/Scripts/services/hasPermissionsToReadPrices.js
@@ -1,27 +1,14 @@
 angular.module('virtoCommerce.orderModule')
     .factory('virtoCommerce.orderModule.hasPermissionsToReadPrices', ['platformWebApp.authService', 'virtoCommerce.orderModule.knownLimitResponseScopes',
         function (authService, knownLimitResponseScopes) {
-            var cachedPermissionValue = null;
-
             return {
                 check: function () {
-                    if (cachedPermissionValue !== null) {
-                        return cachedPermissionValue;
-                    }
-
                     var limitResponseScope = 'order:read:OrderLimitResponseScope';
                     var limitResponseScopes = _.map(knownLimitResponseScopes.items, function (item) { return item.id; });
                     var isSetOrderLimitResponseScope = authService.checkPermission(limitResponseScope, limitResponseScopes);
                     var hasPermissionReadOrderPrices = authService.checkPermission(limitResponseScope + ':WithPrices');
 
-                    cachedPermissionValue = hasPermissionReadOrderPrices || !isSetOrderLimitResponseScope;
-
-                    return cachedPermissionValue;
-                },
-
-                reCheck: function () {
-                    cachedPermissionValue = null;
-                    return this.check();
+                    return hasPermissionReadOrderPrices || !isSetOrderLimitResponseScope;
                 }
             };
         }]
