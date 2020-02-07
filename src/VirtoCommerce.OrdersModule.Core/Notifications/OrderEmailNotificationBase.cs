c64ac5a9e66a2b8ee394939bd85cb014ef112844
Andrei Kostyrin <kostyrin2@gmail.com>
2020-02-07 16:39:18 +0200

    #vp-1380 add customer to OrderEmailNotificationBase

--- src/VirtoCommerce.OrdersModule.Core/Notifications/OrderEmailNotificationBase.cs
+++ src/VirtoCommerce.OrdersModule.Core/Notifications/OrderEmailNotificationBase.cs
@@ -1,3 +1,4 @@
+using VirtoCommerce.CustomerModule.Core.Model;
 using VirtoCommerce.NotificationsModule.Core.Model;
 using VirtoCommerce.OrdersModule.Core.Model;
 
@@ -6,5 +7,6 @@ namespace VirtoCommerce.OrdersModule.Core.Notifications
     public abstract class OrderEmailNotificationBase : EmailNotification
     {
         public virtual CustomerOrder CustomerOrder { get; set; }
+        public virtual Member Customer { get; set; }
     }
 }
