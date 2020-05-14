eb4a8b6dd77d9e219c1c79def4d005ace05f8ed3
Andrei Kostyrin <kostyrin2@gmail.com>
2020-05-14 14:12:52 +0200

    #VP-2046 refactor unit test

--- tests/VirtoCommerce.OrdersModule.Tests/CustomerOrderServiceUnitTests.cs
+++ tests/VirtoCommerce.OrdersModule.Tests/CustomerOrderServiceUnitTests.cs
@@ -1,7 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
-using Castle.Core.Logging;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
@@ -17,7 +18,6 @@ using VirtoCommerce.PaymentModule.Core.Model.Search;
 using VirtoCommerce.PaymentModule.Core.Services;
 using VirtoCommerce.Platform.Caching;
 using VirtoCommerce.Platform.Core.Caching;
-using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.Platform.Core.Domain;
 using VirtoCommerce.Platform.Core.Events;
 using VirtoCommerce.ShippingModule.Core.Model.Search;
@@ -53,7 +53,7 @@ namespace VirtoCommerce.OrdersModule.Tests
             _paymentMethodsSearchServiceMock = new Mock<IPaymentMethodsSearchService>();
             _platformMemoryCacheMock = new Mock<IPlatformMemoryCache>();
             _cacheEntryMock = new Mock<ICacheEntry>();
-            
+            _cacheEntryMock.SetupGet(c => c.ExpirationTokens).Returns(new List<IChangeToken>());
         }
 
         [Fact]
@@ -61,69 +61,40 @@ namespace VirtoCommerce.OrdersModule.Tests
         {
             //Arrange
             var id = Guid.NewGuid().ToString();
-            var fakeOrder = GetFakeCustomerOrder(id);
-            var fakeOrderEntity = (CustomerOrderEntity)AbstractTypeFactory<CustomerOrderEntity>.TryCreateInstance().FromModel(fakeOrder, new PrimaryKeyResolvingMap());
+            var newOrder = new CustomerOrder { Id = id };
+            var service = GetCustomerOrderServiceWithPlatformMemoryCache();
+
+            //Act
+            var nullOrder = await service.GetByIdAsync(id);
+            await service.SaveChangesAsync(new[] { newOrder });
+            var order = await service.GetByIdAsync(id);
+
+            //Assert
+            Assert.NotEqual(nullOrder, order);
+        }
+
+        
+        private CustomerOrderService GetCustomerOrderServiceWithPlatformMemoryCache()
+        {
             var memoryCache = new MemoryCache(Options.Create(new MemoryCacheOptions()));
             var platformMemoryCache = new PlatformMemoryCache(memoryCache, Options.Create(new CachingOptions()), new Mock<ILogger<PlatformMemoryCache>>().Object);
-            var service = GetCustomerOrderService(platformMemoryCache);
-            _cacheEntryMock.SetupGet(c => c.ExpirationTokens).Returns(new List<IChangeToken>());
-            var cacheKey = CacheKey.With(service.GetType(), nameof(service.GetByIdsAsync), string.Join("-", new[] { id }), null);
 
-            //Act
-            var nullOrder = await service.GetByIdAsync(id);
-            var cachedNullOrder = (CustomerOrder[])platformMemoryCache.Get(cacheKey);
-            await service.SaveChangesAsync(new[] { fakeOrder });
-            _orderRepositoryMock
-                .Setup(x => x.GetCustomerOrdersByIdsAsync(new[] {id}, null))
-                .ReturnsAsync(new [] { fakeOrderEntity });
-            var order = await service.GetByIdAsync(id);
-            //need to call again for verify calling times
-            await service.GetByIdAsync(id);
-            var cachedOrder = (CustomerOrder[])platformMemoryCache.Get(cacheKey);
+            var options = new Mock<DbContextOptions<OrderDbContext>>();
+            options.Setup(x => x.ContextType).Returns(typeof(DbContext));
+            var orderDbConextMock = new Mock<OrderDbContext>(options.Object);
+            orderDbConextMock.Setup(x => x.Set<CustomerOrderEntity>())
+                .Returns(GetQueryableMockDbSet(new List<CustomerOrderEntity>()));
+            var orderRepository = new FakeOrderRepository(orderDbConextMock.Object, _unitOfWorkMock.Object);
 
-            //Assert
-            Assert.Null(nullOrder);
-            Assert.Empty(cachedNullOrder);
-            Assert.NotNull(order);
-            Assert.NotEmpty(cachedOrder);
-            _orderRepositoryMock.Verify(x => x.GetCustomerOrdersByIdsAsync(It.IsAny<string[]>(), It.IsAny<string>()), Times.AtMost(3));
+            return GetCustomerOrderService(platformMemoryCache, orderRepository);
         }
-
-        [Fact]
-        public async Task GetByIdsAsync_GetThenSaveOrder_ReturnNotCachedOrder()
-        {
-            //Arrange
-            var id = Guid.NewGuid().ToString();
-            var fakeOrder = GetFakeCustomerOrder(id);
-            var fakeOrderEntity = (CustomerOrderEntity)AbstractTypeFactory<CustomerOrderEntity>.TryCreateInstance().FromModel(fakeOrder, new PrimaryKeyResolvingMap());
-            var service = GetCustomerOrderService();
-            _cacheEntryMock.SetupGet(c => c.ExpirationTokens).Returns(new List<IChangeToken>());
-            var cacheKey = CacheKey.With(service.GetType(), nameof(service.GetByIdsAsync), string.Join("-", new[] { id }), null);
-            _platformMemoryCacheMock.Setup(pmc => pmc.CreateEntry(cacheKey)).Returns(_cacheEntryMock.Object);
-
-            //Act
-            var nullOrder = await service.GetByIdAsync(id);
-            await service.SaveChangesAsync(new[] { fakeOrder });
-            _orderRepositoryMock
-                .Setup(x => x.GetCustomerOrdersByIdsAsync(new[] { id }, null))
-                .ReturnsAsync(new[] { fakeOrderEntity });
-            var order = await service.GetByIdAsync(id);
-            //need to call again for verify calling times
-            await service.GetByIdAsync(id);
-
-            //Assert
-            Assert.Null(nullOrder);
-            Assert.NotNull(order);
-            _orderRepositoryMock.Verify(x => x.GetCustomerOrdersByIdsAsync(It.IsAny<string[]>(), It.IsAny<string>()), Times.AtLeast(4));
-        }
-
-
+        
         private CustomerOrderService GetCustomerOrderService()
         {
-            return GetCustomerOrderService(_platformMemoryCacheMock.Object);
+            return GetCustomerOrderService(_platformMemoryCacheMock.Object, _orderRepositoryMock.Object);
         }
 
-        private CustomerOrderService GetCustomerOrderService(IPlatformMemoryCache platformMemoryCache)
+        private CustomerOrderService GetCustomerOrderService(IPlatformMemoryCache platformMemoryCache, IOrderRepository orderRepository)
         {
             _shippingMethodsSearchServiceMock
                 .Setup(x => x.SearchShippingMethodsAsync(It.IsAny<ShippingMethodsSearchCriteria>()))
@@ -134,7 +105,7 @@ namespace VirtoCommerce.OrdersModule.Tests
                 .ReturnsAsync(new PaymentMethodsSearchResult());
 
             _orderRepositoryMock.Setup(ss => ss.UnitOfWork).Returns(_unitOfWorkMock.Object);
-            return new CustomerOrderService(() => _orderRepositoryMock.Object,
+            return new CustomerOrderService(() => orderRepository,
                 _uniqueNumberGeneratorMock.Object,
                 _storeServiceMock.Object,
                 _eventPublisherMock.Object,
@@ -144,12 +115,78 @@ namespace VirtoCommerce.OrdersModule.Tests
                 platformMemoryCache);
         }
 
-        private CustomerOrder GetFakeCustomerOrder(string id)
+        
+
+        private static DbSet<T> GetQueryableMockDbSet<T>(List<T> sourceList) where T : class
         {
-            return new CustomerOrder
-            {
-                Id = id
-            };
+            var queryable = sourceList.AsQueryable();
+
+            var dbSet = new Mock<DbSet<T>>();
+            dbSet.As<IQueryable<T>>().Setup(m => m.Provider).Returns(queryable.Provider);
+            dbSet.As<IQueryable<T>>().Setup(m => m.Expression).Returns(queryable.Expression);
+            dbSet.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(queryable.ElementType);
+            dbSet.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(() => queryable.GetEnumerator());
+            dbSet.Setup(d => d.Add(It.IsAny<T>())).Callback<T>((s) => sourceList.Add(s));
+
+            return dbSet.Object;
+        }
+    }
+
+
+    class FakeOrderRepository : IOrderRepository
+    {
+        private readonly OrderDbContext _orderDbContext;
+
+        public FakeOrderRepository(OrderDbContext dbContext, IUnitOfWork unitOfWork)
+        {
+            _orderDbContext = dbContext;
+            UnitOfWork = unitOfWork;
+        }
+        public void Dispose()
+        {
+            _orderDbContext.Dispose();
+        }
+
+        public void Attach<T>(T item) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add<T>(T item) where T : class
+        {
+            _orderDbContext.Set<T>().Add(item);
+        }
+
+        public void Update<T>(T item) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove<T>(T item) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public IUnitOfWork UnitOfWork { get; }
+
+        public IQueryable<CustomerOrderEntity> CustomerOrders => _orderDbContext.Set<CustomerOrderEntity>();
+        public IQueryable<ShipmentEntity> Shipments { get; }
+        public IQueryable<PaymentInEntity> InPayments { get; }
+        public IQueryable<AddressEntity> Addresses { get; }
+        public IQueryable<LineItemEntity> LineItems { get; }
+        public Task<CustomerOrderEntity[]> GetCustomerOrdersByIdsAsync(string[] ids, string responseGroup = null)
+        {
+            return Task.FromResult(CustomerOrders.Where(x => ids.Contains(x.Id)).ToArray());
+        }
+
+        public Task<PaymentInEntity[]> GetPaymentsByIdsAsync(string[] ids, string responseGroup = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveOrdersByIdsAsync(string[] ids)
+        {
+            throw new NotImplementedException();
         }
     }
 }
