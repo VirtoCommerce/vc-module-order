1d4d78c26244b053ba5508f4a4a17a93e9d0de18
alexandr andreev <alivemenone@gmail.com>
2020-07-03 15:15:15 +0300

    VP-3496 Add pagination to order history blade (#170)
    * Add method to controller to search order history
    * Add infinite scrolling to order history blade

---
+++ src/VirtoCommerce.OrdersModule.Core/Model/Search/CustomerOrderHistorySearchCriteria.cs
@@ -0,0 +1,9 @@
+using VirtoCommerce.Platform.Core.Common;
+
+namespace VirtoCommerce.OrdersModule.Core.Model.Search
+{
+    public class CustomerOrderHistorySearchCriteria : SearchCriteriaBase
+    {
+        public string OrderId { get; set; }
+    }
+}
--- src/VirtoCommerce.OrdersModule.Web/Scripts/blades/customerOrder-change-log.js
+++ src/VirtoCommerce.OrdersModule.Web/Scripts/blades/customerOrder-change-log.js
@@ -1,16 +1,81 @@
 angular.module('virtoCommerce.orderModule')
-.controller('virtoCommerce.orderModule.customerOrderChangeLogController', ['$scope', 'virtoCommerce.orderModule.order_res_customerOrders', 
-function ($scope, customerOrders) {
+    .controller('virtoCommerce.orderModule.customerOrderChangeLogController', ['$scope', 'virtoCommerce.orderModule.order_res_customerOrders', 'platformWebApp.uiGridHelper', 'platformWebApp.bladeUtils', '$timeout', function ($scope, customerOrders, uiGridHelper, bladeUtils, $timeout) {
+        var blade = $scope.blade;
 
-    customerOrders.getOrderChanges({
-        id: $scope.blade.orderId
-    }, function (data) {
-        $scope.blade.isLoading = false;
-        $scope.blade.currentEntities = data;
-    });
+        //pagination settings
+        $scope.pageSettings = {};
+        $scope.pageSettings.currentPage = 1;
+        $scope.pageSettings.itemsPerPageCount = 20;
+        $scope.pageSettings.totalItems = 0;
 
-    // ui-grid
-    $scope.setGridOptions = function (gridOptions) {
-        $scope.gridOptions = gridOptions;
-    };
+        blade.refresh = function () {
+            blade.isLoading = true;
+
+            if ($scope.pageSettings.currentPage !== 1) {
+                $scope.pageSettings.currentPage = 1;
+            }
+
+            customerOrders.searchOrderChanges(getSearchCriteria(), (data) => {
+                blade.isLoading = false;
+                $scope.pageSettings.totalItems = data.totalCount;
+                blade.currentEntities = data.results;
+                $scope.hasMore = data.results.length === $scope.pageSettings.itemsPerPageCount;
+
+                if ($scope.gridApi) {
+                    $scope.gridApi.infiniteScroll.resetScroll(true, true);
+                    $scope.gridApi.infiniteScroll.dataLoaded();
+                }
+            });
+        };
+
+        function showMore() {
+            if ($scope.hasMore) {
+                ++$scope.pageSettings.currentPage;
+                $scope.gridApi.infiniteScroll.saveScrollPercentage();
+                blade.isLoading = true;
+
+                customerOrders.searchOrderChanges(getSearchCriteria(), (data) => {
+                    blade.isLoading = false;
+                    $scope.pageSettings.totalItems = data.totalCount;
+                    blade.currentEntities = blade.currentEntities.concat(data.results);
+                    $scope.hasMore = data.results.length === $scope.pageSettings.itemsPerPageCount;
+                    $scope.gridApi.infiniteScroll.dataLoaded();
+                });
+            }
+        }
+
+        function getSearchCriteria() {
+            return {
+                orderId: blade.orderId,
+                sort: uiGridHelper.getSortExpression($scope),
+                skip: ($scope.pageSettings.currentPage - 1) * $scope.pageSettings.itemsPerPageCount,
+                take: $scope.pageSettings.itemsPerPageCount
+            };
+        }
+
+        var filter = blade.filter = $scope.filter = {};
+        filter.criteriaChanged = function () {
+            if ($scope.pageSettings.currentPage > 1) {
+                $scope.pageSettings.currentPage = 1;
+            } else {
+                blade.refresh();
+            }
+        };
+
+        // ui-grid
+        $scope.setGridOptions = function (gridOptions) {
+            bladeUtils.initializePagination($scope, true);
+
+            uiGridHelper.initialize($scope, gridOptions, function (gridApi) {
+                //update gridApi for current grid
+                $scope.gridApi = gridApi;
+                uiGridHelper.bindRefreshOnSortChanged($scope);
+                $scope.gridApi.infiniteScroll.on.needLoadMoreData($scope, showMore);
+
+            });
+
+            $timeout(function () { blade.refresh(); });
+        };
+
+        blade.refresh();
 }]);
