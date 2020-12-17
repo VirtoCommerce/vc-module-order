962bab18d780bed67abaa87209877a61ede64317
Andrei Kolchanov <akak1977@users.noreply.github.com>
2020-12-17 16:04:23 +0500

    VP-6222: Correct orders filtering by "Assigned to" selector (#205)
    * VP-6222: Correct orders filtering by "Assigned to" selector
    
    * Use member Id claim
    
    Co-authored-by: Aleksandr Andreev <alivemenone@gmail.com>

--- src/VirtoCommerce.OrdersModule.Web/Authorization/OrderAuthorizationHandler.cs
+++ src/VirtoCommerce.OrdersModule.Web/Authorization/OrderAuthorizationHandler.cs
@@ -1,4 +1,5 @@
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -16,13 +17,18 @@ namespace VirtoCommerce.OrdersModule.Web.Authorization
     {
         public OrderAuthorizationRequirement(string permission)
             : base(permission)
-        {          
+        {
         }
     }
 
     public sealed class OrderAuthorizationHandler : PermissionAuthorizationHandlerBase<OrderAuthorizationRequirement>
     {
+        //VP-6222 Fix permission scope "Only for order responsible"
+        //Copy of PlatformConstants.Security.Claims.MemberIdClaimType. Copied to reduce platform version dependency
+        public const string MemberIdClaimType = "memberId";
+
         private readonly MvcNewtonsoftJsonOptions _jsonOptions;
+
         public OrderAuthorizationHandler(IOptions<MvcNewtonsoftJsonOptions> jsonOptions)
         {
             _jsonOptions = jsonOptions.Value;
@@ -31,7 +37,7 @@ namespace VirtoCommerce.OrdersModule.Web.Authorization
         protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, OrderAuthorizationRequirement requirement)
         {
             await base.HandleRequirementAsync(context, requirement);
-            
+
             if (!context.HasSucceeded)
             {
                 var userPermission = context.User.FindPermission(requirement.Permission, _jsonOptions.SerializerSettings);
@@ -43,15 +49,19 @@ namespace VirtoCommerce.OrdersModule.Web.Authorization
 
                     if (context.Resource is OrderOperationSearchCriteriaBase criteria)
                     {
-                        criteria.StoreIds = allowedStoreIds;                       
+                        criteria.StoreIds = allowedStoreIds;
                         if (onlyResponsibleScope != null)
                         {
-                            criteria.EmployeeId = context.User.Identity.Name;
-                        }                      
-                        context.Succeed(requirement);                        
-                    }                  
+                            var identityName = context.User.Identity.Name;
+                            var memberId = context.User.FindFirstValue(MemberIdClaimType);
+                            criteria.EmployeeId = memberId ?? identityName;
+                        }
+
+                        context.Succeed(requirement);
+                    }
+
                     if (context.Resource is CustomerOrder order)
-                    {                     
+                    {
                         if (allowedStoreIds.Contains(order.StoreId) || (onlyResponsibleScope != null && order.EmployeeId.EqualsInvariant(context.User.Identity.Name)))
                         {
                             context.Succeed(requirement);
@@ -69,18 +79,21 @@ namespace VirtoCommerce.OrdersModule.Web.Authorization
                     {
                         criteria.ResponseGroup = CustomerOrderResponseGroup.Full.ToString();
                     }
+
                     criteria.ResponseGroup = EnumUtility.SafeRemoveFlagFromEnumString(criteria.ResponseGroup, CustomerOrderResponseGroup.WithPrices);
                     //Do not allow pass empty response group into services because that can leads to use default response group CustomerOrderResponseGroup.Full
                     if (string.IsNullOrEmpty(criteria.ResponseGroup))
                     {
                         criteria.ResponseGroup = CustomerOrderResponseGroup.Default.ToString();
                     }
+
                     context.Succeed(requirement);
                 }
+
                 if (context.Resource is CustomerOrder order)
                 {
                     order.ReduceDetails((CustomerOrderResponseGroup.Full & ~CustomerOrderResponseGroup.WithPrices).ToString());
-                }           
+                }
             }
         }
     }
