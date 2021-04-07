935c2004fb63da1e53eb0ec58c59c0ff27c6e009
Alexander Morogov <asmorogov@gmail.com>
2021-04-07 15:53:49 +0200

    PT-670: Fix filter icon

--- src/VirtoCommerce.OrdersModule.Web/Scripts/blades/filter-detail.js
+++ src/VirtoCommerce.OrdersModule.Web/Scripts/blades/filter-detail.js
@@ -73,7 +73,7 @@ angular.module('virtoCommerce.orderModule')
             return !angular.equals(blade.currentEntity, blade.origEntity);
         };
 
-        blade.headIcon = 'fa-filter';
+        blade.headIcon = 'fa fa-filter';
 
         blade.toolbarCommands = [
                 {
