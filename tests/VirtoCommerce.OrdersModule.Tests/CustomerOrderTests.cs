951f373aa84535e7164118a80475fb0cc859f906
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2021-06-07 16:24:07 +0200

    PT-2067 changed ShipmentItem-LineItem compare logic (#241)
    * PT-2067 changed shipmentItem-lineItem compare logic
    
    * PT-2067 added NRE check

---
+++ tests/VirtoCommerce.OrdersModule.Tests/CustomerOrderTests.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using VirtoCommerce.OrdersModule.Core.Model;
+using VirtoCommerce.OrdersModule.Data.Model;
+using VirtoCommerce.Platform.Core.Common;
+using Xunit;
+
+
+namespace VirtoCommerce.OrdersModule.Tests
+{
+    public class CustomerOrderTests
+    {
+        [Fact]
+        public void FromModel_MultipleShipments_ShipmentItemsLinkedCorrectly()
+        {
+            // Arrange
+            // two line items, two shipments, each shipment has one shipmentItem corresponding with order lineItem
+            var lineItem1 = new LineItem
+            {
+                ProductId = Guid.NewGuid().ToString(),
+                Quantity = 2
+            };
+            var lineItem2 = new LineItem
+            {
+                ProductId = Guid.NewGuid().ToString(),
+                Quantity = 5
+            };
+
+            var order = new CustomerOrder
+            {
+                Items = new List<LineItem>
+                {
+                    lineItem1,
+                    lineItem2
+                },
+                Shipments = new List<Shipment>
+                {
+                    new Shipment
+                    {
+                        Items = new List<ShipmentItem>
+                        {
+                            new ShipmentItem
+                            {
+                                LineItem = lineItem1
+                            }
+                        }
+                    },
+                    new Shipment()
+                    {
+                        Items = new List<ShipmentItem>
+                        {
+                            new ShipmentItem
+                            {
+                                LineItem = lineItem2
+                            }
+                        }
+                    }
+                }
+            };
+
+            // Act
+            var orderEntity = AbstractTypeFactory<CustomerOrderEntity>
+                .TryCreateInstance()
+                .FromModel(order, new PrimaryKeyResolvingMap()) as CustomerOrderEntity;
+
+            // Assert
+            var shipmentLineItem1 = orderEntity.Shipments.First().Items.First().LineItem;
+            var shipmentLineItem2 = orderEntity.Shipments.Last().Items.First().LineItem;
+            var equals = ReferenceEquals(shipmentLineItem1, shipmentLineItem2);
+            Assert.False(equals);
+        }
+    }
+}
