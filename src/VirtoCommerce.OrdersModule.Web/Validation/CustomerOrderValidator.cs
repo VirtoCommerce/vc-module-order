cd47f3032c76f60787b4c3d5335a9b2f5a675c40
Alexander Morogov <asmorogov@gmail.com>
2021-07-08 14:59:05 +0200

    PT-899: Remove ScalePrecision methods

--- src/VirtoCommerce.OrdersModule.Web/Validation/CustomerOrderValidator.cs
+++ src/VirtoCommerce.OrdersModule.Web/Validation/CustomerOrderValidator.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using FluentValidation;
 using VirtoCommerce.OrdersModule.Core.Model;
 
@@ -5,41 +7,42 @@ namespace VirtoCommerce.OrdersModule.Web.Validation
 {
     public class CustomerOrderValidator : AbstractValidator<CustomerOrder>
     {
-        public CustomerOrderValidator()
+        public CustomerOrderValidator(IEnumerable<IValidator<LineItem>> lineItemValidators,
+            IEnumerable<IValidator<Shipment>> shipmentValidators,
+            IValidator<PaymentIn> paymentInValidator)
+        {
+            SetDefaultRules();
+
+            if (lineItemValidators.Any())
+            {
+                RuleForEach(order => order.Items).SetValidator(lineItemValidators.Last(), "default");
+            }
+
+            if (shipmentValidators.Any())
+            {
+                RuleForEach(order => order.Shipments).SetValidator(shipmentValidators.Last(), "default");
+            }
+
+            RuleForEach(order => order.InPayments).SetValidator(paymentInValidator);
+        }
+
+        protected void SetDefaultRules()
         {
             RuleFor(order => order.Number).MaximumLength(128);
-
             RuleFor(order => order.CustomerId).NotNull().NotEmpty().MaximumLength(64);
             RuleFor(order => order.CustomerName).MaximumLength(255);
-
             RuleFor(order => order.StoreId).NotNull().NotEmpty().MaximumLength(64);
             RuleFor(order => order.StoreName).MaximumLength(255);
-
             RuleFor(order => order.ChannelId).MaximumLength(64);
             RuleFor(order => order.OrganizationId).MaximumLength(64);
             RuleFor(order => order.OrganizationName).MaximumLength(255);
-
             RuleFor(order => order.EmployeeId).MaximumLength(64);
             RuleFor(order => order.EmployeeName).MaximumLength(255);
-
             RuleFor(order => order.SubscriptionId).MaximumLength(64);
             RuleFor(order => order.SubscriptionNumber).MaximumLength(64);
-
             RuleFor(order => order.LanguageCode).MaximumLength(16);
             RuleFor(order => order.ShoppingCartId).MaximumLength(128);
             RuleFor(order => order.PurchaseOrderNumber).MaximumLength(128);
-
-            RuleFor(order => order.DiscountAmount).ScalePrecision(4, 19);
-            RuleFor(order => order.TaxTotal).ScalePrecision(4, 19);
-            RuleFor(order => order.Total).ScalePrecision(4, 19);
-            RuleFor(order => order.SubTotal).ScalePrecision(4, 19);
-            RuleFor(order => order.SubTotalWithTax).ScalePrecision(4, 19);
-            RuleFor(order => order.ShippingTotal).ScalePrecision(4, 19);
-            RuleFor(order => order.ShippingTotalWithTax).ScalePrecision(4, 19);
-            RuleFor(order => order.PaymentTotal).ScalePrecision(4, 19);
-            RuleFor(order => order.PaymentTotalWithTax).ScalePrecision(4, 19);
-            RuleFor(order => order.DiscountTotal).ScalePrecision(4, 19);
-            RuleFor(order => order.DiscountTotalWithTax).ScalePrecision(4, 19);
         }
     }
 }
--- src/VirtoCommerce.OrdersModule.Web/Validation/PaymentInValidator.cs
+++ src/VirtoCommerce.OrdersModule.Web/Validation/PaymentInValidator.cs
@@ -1,9 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
 using FluentValidation;
 using VirtoCommerce.OrdersModule.Core.Model;
-using VirtoCommerce.OrdersModule.Data.Model;
 
 namespace VirtoCommerce.OrdersModule.Web.Validation
 {
@@ -11,27 +7,18 @@ namespace VirtoCommerce.OrdersModule.Web.Validation
     {
         public PaymentInValidator()
         {
+            SetDefaultRules();
+        }
+
+        protected void SetDefaultRules()
+        {
             RuleFor(payment => payment.OrganizationId).MaximumLength(64);
             RuleFor(payment => payment.OrganizationName).MaximumLength(255);
-
             RuleFor(payment => payment.CustomerId).NotNull().NotEmpty().MaximumLength(64);
             RuleFor(payment => payment.CustomerName).MaximumLength(255);
-
             RuleFor(payment => payment.Purpose).MaximumLength(1024);
             RuleFor(payment => payment.GatewayCode).MaximumLength(64);
-
             RuleFor(payment => payment.TaxType).MaximumLength(64);
-
-            RuleFor(payment => payment.Price).ScalePrecision(4, 19);
-            RuleFor(payment => payment.PriceWithTax).ScalePrecision(4, 19);
-
-            RuleFor(payment => payment.DiscountAmount).ScalePrecision(4, 19);
-            RuleFor(payment => payment.DiscountAmountWithTax).ScalePrecision(4, 19);
-
-            RuleFor(payment => payment.Total).ScalePrecision(4, 19);
-            RuleFor(payment => payment.TotalWithTax).ScalePrecision(4, 19);
-
-            RuleFor(payment => payment.TaxTotal).ScalePrecision(4, 19);
         }
     }
 }
