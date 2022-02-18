d20661d44d7f408150fdd5efe552ff8fc7ef197c
getmansky <47969278+getmansky@users.noreply.github.com>
2022-02-18 09:22:30 +0300

    PT-6527: Partial search index update support (#284)
    * Partial index update support
    
    * Using beta search module nuget
    
    * Using release version of the search module Nuget

--- src/VirtoCommerce.OrdersModule.Data/Handlers/IndexCustomerOrderChangedEventHandler.cs
+++ src/VirtoCommerce.OrdersModule.Data/Handlers/IndexCustomerOrderChangedEventHandler.cs
@@ -1,14 +1,18 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using VirtoCommerce.OrdersModule.Core;
 using VirtoCommerce.OrdersModule.Core.Events;
 using VirtoCommerce.OrdersModule.Core.Extensions;
+using VirtoCommerce.OrdersModule.Data.Search.Indexed;
 using VirtoCommerce.Platform.Core.Events;
+using VirtoCommerce.Platform.Core.Jobs;
 using VirtoCommerce.Platform.Core.Settings;
 using VirtoCommerce.SearchModule.Core.Model;
 using VirtoCommerce.SearchModule.Data.BackgroundJobs;
+using VirtoCommerce.SearchModule.Data.Services;
 
 namespace VirtoCommerce.OrdersModule.Data.Handlers
 {
@@ -16,11 +20,13 @@ namespace VirtoCommerce.OrdersModule.Data.Handlers
     {
         private readonly ISettingsManager _settingsManager;
         private readonly IConfiguration _configuration;
+        private readonly IEnumerable<IndexDocumentConfiguration> _indexingConfigurations;
 
-        public IndexCustomerOrderChangedEventHandler(ISettingsManager settingsManager, IConfiguration configuration)
+        public IndexCustomerOrderChangedEventHandler(ISettingsManager settingsManager, IConfiguration configuration, IEnumerable<IndexDocumentConfiguration> indexingConfigurations)
         {
             _settingsManager = settingsManager;
             _configuration = configuration;
+            _indexingConfigurations = indexingConfigurations;
         }
 
         public async Task Handle(OrderChangedEvent message)
@@ -35,7 +41,8 @@ namespace VirtoCommerce.OrdersModule.Data.Handlers
                 .Select(x => new IndexEntry { Id = x.OldEntry.Id, EntryState = x.EntryState, Type = ModuleConstants.OrderIndexDocumentType })
                 .ToArray() ?? Array.Empty<IndexEntry>();
 
-            IndexingJobs.EnqueueIndexAndDeleteDocuments(indexEntries);
+            IndexingJobs.EnqueueIndexAndDeleteDocuments(indexEntries,
+                JobPriority.Normal, _indexingConfigurations.GetBuildersForProvider(typeof(CustomerOrderChangesProvider)).ToList());
         }
     }
 }
