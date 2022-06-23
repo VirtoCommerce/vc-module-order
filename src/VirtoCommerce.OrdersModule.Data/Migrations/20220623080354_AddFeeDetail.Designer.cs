291ec62fe770304592a53c7b2b85aad1529beea9
Belyakov Sergey <Belyakov.Sergey@virtoway.com>
2022-06-23 11:09:16 +0300

    VM-789: Add Migration

--- src/VirtoCommerce.OrdersModule.Data/Migrations/20220623080354_AddFeeDetail.Designer.cs
+++ src/VirtoCommerce.OrdersModule.Data/Migrations/20220623080354_AddFeeDetail.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VirtoCommerce.OrdersModule.Data.Repositories;
 
@@ -11,9 +12,10 @@ using VirtoCommerce.OrdersModule.Data.Repositories;
 namespace VirtoCommerce.OrdersModule.Data.Migrations
 {
     [DbContext(typeof(OrderDbContext))]
-    partial class OrderDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220623080354_AddFeeDetail")]
+    partial class AddFeeDetail
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
