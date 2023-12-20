055388ceb91d8dd92e726e424019a4fac895406a
Oleg Zhuk <zhukoo@gmail.com>
2023-12-20 14:34:46 +0200

    PT-14675: Add Shipment Search Service (#394)
    feat: Add Shipment Search Service (IShipmentSearchService) and Rest API (api/order/shipments) to search shipments,

--- src/VirtoCommerce.OrdersModule.Web/Controllers/Api/OrderModuleShipmentsController.cs
+++ src/VirtoCommerce.OrdersModule.Web/Controllers/Api/OrderModuleShipmentsController.cs
@@ -3,7 +3,9 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using VirtoCommerce.OrdersModule.Core;
 using VirtoCommerce.OrdersModule.Core.Model;
+using VirtoCommerce.OrdersModule.Core.Model.Search;
 using VirtoCommerce.OrdersModule.Core.Services;
+using VirtoCommerce.OrdersModule.Data.Authorization;
 
 namespace VirtoCommerce.OrdersModule.Web.Controllers.Api
 {
@@ -12,10 +14,16 @@ namespace VirtoCommerce.OrdersModule.Web.Controllers.Api
     public class OrderModuleShipmentsController : Controller
     {
         private readonly IShipmentService _shipmentService;
+        private readonly IShipmentSearchService _shipmentSearchService;
+        private readonly IAuthorizationService _authorizationService;
 
-        public OrderModuleShipmentsController(IShipmentService shipmentService)
+        public OrderModuleShipmentsController(IShipmentService shipmentService,
+            IShipmentSearchService shipmentSearchService,
+            IAuthorizationService authorizationService)
         {
             _shipmentService = shipmentService;
+            _shipmentSearchService = shipmentSearchService;
+            _authorizationService = authorizationService;
         }
 
         [HttpPost]
@@ -25,6 +33,25 @@ namespace VirtoCommerce.OrdersModule.Web.Controllers.Api
             await _shipmentService.SaveChangesAsync(new[] { shipment });
 
             return Ok();
+        }
+
+        /// <summary>
+        /// Search shipments by given criteria
+        /// </summary>
+        /// <param name="criteria">criteria</param>
+        [HttpPost]
+        [Route("search")]
+        public async Task<ActionResult<ShipmentSearchResult>> SearchOrderShipments([FromBody] ShipmentSearchCriteria criteria)
+        {
+            var authorizationResult = await _authorizationService.AuthorizeAsync(User, criteria, new OrderAuthorizationRequirement(ModuleConstants.Security.Permissions.Read));
+            if (!authorizationResult.Succeeded)
+            {
+                return Forbid();
+            }
+
+            var result = await _shipmentSearchService.SearchAsync(criteria);
+
+            return Ok(result);
         }
     }
 }
