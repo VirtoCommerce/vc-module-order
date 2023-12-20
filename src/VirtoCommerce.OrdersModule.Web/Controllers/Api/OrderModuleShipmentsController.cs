4c8b9030a265e9bc46ef1935a9b98907b831490b
Oleg Zhuk <zhukoo@gmail.com>
2023-12-20 12:39:43 +0200

    revert back POST api/order/shipments

--- src/VirtoCommerce.OrdersModule.Web/Controllers/Api/OrderModuleShipmentsController.cs
+++ src/VirtoCommerce.OrdersModule.Web/Controllers/Api/OrderModuleShipmentsController.cs
@@ -2,6 +2,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using VirtoCommerce.OrdersModule.Core;
+using VirtoCommerce.OrdersModule.Core.Model;
 using VirtoCommerce.OrdersModule.Core.Model.Search;
 using VirtoCommerce.OrdersModule.Core.Services;
 using VirtoCommerce.OrdersModule.Data.Authorization;
@@ -12,16 +13,27 @@ namespace VirtoCommerce.OrdersModule.Web.Controllers.Api
     [Authorize]
     public class OrderModuleShipmentsController : Controller
     {
+        private readonly IShipmentService _shipmentService;
         private readonly IShipmentSearchService _shipmentSearchService;
         private readonly IAuthorizationService _authorizationService;
 
-        public OrderModuleShipmentsController(IShipmentSearchService shipmentSearchService,
+        public OrderModuleShipmentsController(IShipmentService shipmentService,
+            IShipmentSearchService shipmentSearchService,
             IAuthorizationService authorizationService)
         {
+            _shipmentService = shipmentService;
             _shipmentSearchService = shipmentSearchService;
             _authorizationService = authorizationService;
         }
 
+        [HttpPost]
+        [Authorize(ModuleConstants.Security.Permissions.UpdateShipments)]
+        public async Task<ActionResult> UpdateShipment([FromBody] Shipment shipment)
+        {
+            await _shipmentService.SaveChangesAsync(new[] { shipment });
+
+            return Ok();
+        }
 
         /// <summary>
         /// Search shipments by given criteria
