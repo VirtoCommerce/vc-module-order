3d787e632ef5d2ddd2e22e70386da61ae1a304e0
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2024-02-01 11:32:22 +0200

    VCST-184: add order aggregations (#399)
    Co-authored-by: Oleg Zhuk <zhukoo@gmail.com>

--- src/VirtoCommerce.OrdersModule.Core/Model/Search/CustomerOrderIndexedSearchCriteria.cs
+++ src/VirtoCommerce.OrdersModule.Core/Model/Search/CustomerOrderIndexedSearchCriteria.cs
@@ -1,10 +1,7 @@
-using System;
-using VirtoCommerce.Platform.Core.Common;
-
 namespace VirtoCommerce.OrdersModule.Core.Model.Search
 {
-    [Obsolete("Use CustomerOrderSearchCriteria", DiagnosticId = "VC0008", UrlFormat = "https://docs.virtocommerce.org/products/products-virto3-versions/")]
-    public class CustomerOrderIndexedSearchCriteria : SearchCriteriaBase
+    public class CustomerOrderIndexedSearchCriteria : CustomerOrderSearchCriteria
     {
+        public string Facet { get; set; }
     }
 }
---
+++ src/VirtoCommerce.OrdersModule.Core/Model/Search/OrderAggregationItem.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace VirtoCommerce.OrdersModule.Core.Model.Search
+{
+    public class OrderAggregationItem
+    {
+        /// <summary>
+        /// Gets or sets the aggregation item value
+        /// </summary>
+        public object Value { get; set; }
+
+        /// <summary>
+        /// Gets or sets the aggregation item count
+        /// </summary>
+        public int Count { get; set; }
+
+        /// <summary>
+        /// Gets or sets the flag for aggregation item is applied
+        /// </summary>
+        public bool IsApplied { get; set; }
+
+        /// <summary>
+        /// Gets or sets the collection of the aggregation item labels
+        /// </summary>
+        public IList<OrderAggregationLabel> Labels { get; set; }
+
+        /// <summary>
+        /// Gets or sets the request lower bound for range aggregation value
+        /// </summary>
+        public string RequestedLowerBound { get; set; }
+
+        /// <summary>
+        /// Gets or sets the request lower bound for range aggregation value
+        /// </summary>
+        public string RequestedUpperBound { get; set; }
+
+        /// <summary>
+        /// Is lower bound for range included
+        /// </summary>
+        public bool IncludeLower { get; set; }
+
+        /// <summary>
+        /// Is upper bound for range included
+        /// </summary>
+        public bool IncludeUpper { get; set; }
+    }
+}
