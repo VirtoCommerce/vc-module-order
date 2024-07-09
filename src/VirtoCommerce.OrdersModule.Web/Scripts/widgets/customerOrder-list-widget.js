65b735c0076537566257b4ba0f8b4d9ddd00cee8
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2024-07-09 16:12:00 +0200

    feat: add orders widget to customer blade

---
+++ src/VirtoCommerce.OrdersModule.Web/Scripts/widgets/customerOrder-list-widget.js
@@ -0,0 +1,46 @@
+angular.module('virtoCommerce.orderModule')
+    .controller('virtoCommerce.orderModule.customerOrderListWidgetController', ['$scope', 'platformWebApp.bladeNavigationService', 'virtoCommerce.orderModule.order_res_customerOrders',
+        function ($scope, bladeNavigationService, customerOrders) {
+            var blade = $scope.widget.blade;
+
+            var searchCriteria = {};
+            if (blade.currentEntity) {
+                if (blade.currentEntity.memberType === "Organization") {
+                    searchCriteria.organizationId = blade.currentEntityId;
+                }
+                else {
+                    var account = _.first(blade.currentEntity.securityAccounts)
+                    if (account) {
+                        searchCriteria.customerId = account.id;
+                    }
+                }
+            }
+
+            function refresh() {
+                $scope.ordersCount = '...';
+
+                var countSearchCriteria = {
+                    responseGroup: "Default",
+                    take: 0
+                };
+
+                angular.extend(countSearchCriteria, searchCriteria);
+
+                customerOrders.search(countSearchCriteria, function (data) {
+                    $scope.ordersCount = data.totalCount;
+                });
+            }
+
+            $scope.openBlade = function () {
+                var newBlade = {
+                    id: 'orders',
+                    title: 'orders.blades.customerOrder-list.title',
+                    searchCriteria: searchCriteria,
+                    controller: 'virtoCommerce.orderModule.customerOrderListController',
+                    template: 'Modules/$(VirtoCommerce.Orders)/Scripts/blades/customerOrder-list.tpl.html'
+                };
+                bladeNavigationService.showBlade(newBlade, blade);
+            };
+
+            refresh()
+        }]);
