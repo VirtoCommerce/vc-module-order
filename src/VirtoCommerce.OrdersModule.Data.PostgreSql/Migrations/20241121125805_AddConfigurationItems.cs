0581eb3b0073694eae202ce06aeafa512ea7f4ec
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2024-11-21 14:58:23 +0200

    feat: add migrations

---
+++ src/VirtoCommerce.OrdersModule.Data.PostgreSql/Migrations/20241121125805_AddConfigurationItems.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VirtoCommerce.OrdersModule.Data.PostgreSql.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddConfigurationItems : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsConfigured",
+                table: "OrderLineItem",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "OrderConfigurationItem",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    LineItemId = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    ProductId = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: true),
+                    Name = table.Column<string>(type: "character varying(1024)", maxLength: 1024, nullable: true),
+                    Sku = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: true),
+                    Quantity = table.Column<int>(type: "integer", nullable: false),
+                    ImageUrl = table.Column<string>(type: "character varying(1028)", maxLength: 1028, nullable: true),
+                    CatalogId = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: true),
+                    CategoryId = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ModifiedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    CreatedBy = table.Column<string>(type: "character varying(64)", maxLength: 64, nullable: true),
+                    ModifiedBy = table.Column<string>(type: "character varying(64)", maxLength: 64, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderConfigurationItem", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OrderConfigurationItem_OrderLineItem_LineItemId",
+                        column: x => x.LineItemId,
+                        principalTable: "OrderLineItem",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderConfigurationItem_LineItemId",
+                table: "OrderConfigurationItem",
+                column: "LineItemId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderConfigurationItem");
+
+            migrationBuilder.DropColumn(
+                name: "IsConfigured",
+                table: "OrderLineItem");
+        }
+    }
+}
