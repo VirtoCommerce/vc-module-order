15157d6a7d9425edbb152aa99bc03b15530d75e2
yuskithedeveloper <yuskianywhere@gmail.com>
2025-09-11 09:04:12 -0500

    Refactoring

--- src/VirtoCommerce.OrdersModule.Data/Services/CustomerOrderPaymentService.cs
+++ src/VirtoCommerce.OrdersModule.Data/Services/CustomerOrderPaymentService.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using FluentValidation;
@@ -28,39 +29,23 @@ namespace VirtoCommerce.OrdersModule.Data.Services
         {
             ArgumentNullException.ThrowIfNull(paymentParameters);
 
-            if (string.IsNullOrEmpty(paymentParameters.OrderId))
-            {
-                throw new InvalidOperationException("The 'orderid' parameter must be passed");
-            }
-
-            //some payment method require customer number to be passed and returned. First search customer order by number
-            var searchCriteria = AbstractTypeFactory<CustomerOrderSearchCriteria>.TryCreateInstance();
-            searchCriteria.Number = paymentParameters.OrderId;
-            searchCriteria.ResponseGroup = CustomerOrderResponseGroup.Full.ToString();
-
-            //if order not found by order number search by order id
-            var orders = await customerOrderSearchService.SearchAsync(searchCriteria);
-
-            var customerOrder = orders.Results.FirstOrDefault() ?? await customerOrderService.GetByIdAsync(paymentParameters.OrderId, CustomerOrderResponseGroup.Full.ToString());
-
+            var customerOrder = await GetCustomerOrder(paymentParameters);
             if (customerOrder == null)
             {
                 throw new InvalidOperationException($"Cannot find order with ID {paymentParameters.OrderId}");
             }
 
             var store = await storeService.GetByIdAsync(customerOrder.StoreId, StoreResponseGroup.StoreInfo.ToString());
-
             if (store == null)
             {
                 throw new InvalidOperationException($"Cannot find store with ID {customerOrder.StoreId}");
             }
 
-            //Need to use concrete  payment method if it code passed otherwise use all order payment methods
-            foreach (var inPayment in customerOrder.InPayments.Where(x => x.PaymentMethod != null && (string.IsNullOrEmpty(paymentParameters.PaymentMethodCode) || x.GatewayCode.EqualsIgnoreCase(paymentParameters.PaymentMethodCode))))
+            var inPayments = GetInPayments(customerOrder, paymentParameters);
+            foreach (var inPayment in inPayments)
             {
                 //Each payment method must check that these parameters are addressed to it
                 var paymentMethodValidationResult = inPayment.PaymentMethod.ValidatePostProcessRequest(paymentParameters.Parameters);
-
                 if (!paymentMethodValidationResult.IsSuccess)
                 {
                     continue;
@@ -79,14 +64,12 @@ namespace VirtoCommerce.OrdersModule.Data.Services
                 };
 
                 var paymentMethodPostProcessResult = inPayment.PaymentMethod.PostProcessPayment(paymentMethodRequest);
-
                 if (paymentMethodPostProcessResult == null)
                 {
                     continue;
                 }
 
                 var customerOrderValidationResult = await ValidateAsync(customerOrder);
-
                 if (!customerOrderValidationResult.IsValid)
                 {
                     return new PostProcessPaymentRequestNotValidResult()
@@ -105,6 +88,32 @@ namespace VirtoCommerce.OrdersModule.Data.Services
             }
 
             return new PostProcessPaymentRequestResult { ErrorMessage = "Payment method not found" };
+        }
+
+        protected virtual async Task<CustomerOrder> GetCustomerOrder(PaymentParameters paymentParameters)
+        {
+            if (string.IsNullOrEmpty(paymentParameters.OrderId))
+            {
+                throw new InvalidOperationException("The 'orderid' parameter must be passed");
+            }
+
+            //some payment method require customer number to be passed and returned. First search customer order by number
+            var searchCriteria = AbstractTypeFactory<CustomerOrderSearchCriteria>.TryCreateInstance();
+            searchCriteria.Number = paymentParameters.OrderId;
+            searchCriteria.ResponseGroup = CustomerOrderResponseGroup.Full.ToString();
+
+            //if order not found by order number search by order id
+            var orders = await customerOrderSearchService.SearchAsync(searchCriteria);
+
+            return orders.Results.FirstOrDefault() ?? await customerOrderService.GetByIdAsync(paymentParameters.OrderId, CustomerOrderResponseGroup.Full.ToString());
+        }
+
+        protected virtual IList<PaymentIn> GetInPayments(CustomerOrder customerOrder, PaymentParameters paymentParameters)
+        {
+            //Need to use concrete  payment method if it code passed otherwise use all order payment methods
+            return customerOrder.InPayments
+                .Where(x => x.PaymentMethod != null && (string.IsNullOrEmpty(paymentParameters.PaymentMethodCode) || x.GatewayCode.EqualsIgnoreCase(paymentParameters.PaymentMethodCode)))
+                .ToList();
         }
 
         protected virtual async Task<ValidationResult> ValidateAsync(CustomerOrder customerOrder)
